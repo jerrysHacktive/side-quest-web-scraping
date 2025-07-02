8042acd74d9bd8154be5325c153b9e756556ddce
jerry <devygramm@gmail.com>
2025-07-02 01:08:06 +0100

    Delete route directory

--- route/scrape-route.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const scrapeController = require("../controller/scrape-controller");
-
-//route to run scraping script
-router.get("/", scrapeController.scrapeSites);
-
-module.exports = router;
